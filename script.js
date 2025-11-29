5307a9a7f2ae8f14e61e8e8879213abb7a7f8f17
Hemanth R <91587889+HemanthRaghu96@users.noreply.github.com>
2025-11-29 15:00:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	 if (arr.length <= 1) return 0;
+  let cost = 0;
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+    let first = arr[0];
+    let second = arr[1];
+    let newRope = first + second;
+    cost += newRope;
+    arr.splice(0, 2, newRope);
+  }
+  return cost;
   
 }
 
